696fbb599643b22a6fdb098e129c0362ed452a05
Katherine Rodríguez <116019483+katherine0018@users.noreply.github.com>
2022-12-07 22:52:35 +0000

    paso 4

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-with open('gift_costs.txt') as f:
-  gift_costs = f.read().split('\n')
+"Katherine Rodriguez 22-0018 Refinando código"
 
-gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
 
-total_price = 0
-for cost in gift_costs:
-  if cost > 1000:
-    total_price += cost * 1.16  # agrega impuestos
-  else:
-    total_price += cost
+def costos_lista():
+  with open('gift_costs.txt') as f:
+    gift_costs = f.read().split('\n')
+  gift_costs = [int(c) for c in gift_costs]
 
-print(total_price)
+  # convierte strings a int
+  return gift_costs
+
+
+def total(gift_costs):
+  gift_costs = costos_lista()
+  total_price = 0
+  for cost in gift_costs:
+    if cost > 1000:
+      total_price += cost * 1.16
+      # agrega impuestos
+    else:
+      total_price += cost
+      # los costos menores a 1000 no se le agrega impuesto
+
+  return total_price
+
+
+# llama a los dos funciones y luego imprime el resultado
+def main():
+  print(total(costos_lista))
+
+
+if __name__ == '__main__':
+  main()
