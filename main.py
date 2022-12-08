ea8a33e4ed4ce1ecbc7f5d0f39bd89a7fac33942
Katherine Rodríguez <116019483+katherine0018@users.noreply.github.com>
2022-12-08 22:40:43 +0000

    paso 6

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 # Katherine Rodriguez 22-0018 Refinando código
 
+import sys
+
 
 def costos_lista():
   """Función que devuelve una lista de costos del archivo gift_costs.txt"""
   with open('gift_costs.txt', 'r', encoding='UTF-8') as archive:
-    gift_costs = archive.read().split('\n')
-  gift_costs = [int(c) for c in gift_costs]
+    gift_costs = list(archive)
+  try:
+    gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+  except ValueError:
+    print('Los datos deben ser dígitos.')
+    sys.exit()
 
-  # convierte strings a int
   return gift_costs
 
 
 def total(gift_costs):
-  gift_costs = costos_lista()
+  """Función que suma los precios de la lista de costos para conseguir un total"""
   total_price = 0
   for cost in gift_costs:
     if cost > 1000:
       total_price += cost * 1.16  # agrega impuestos
     else:
-      total_price += cost
-      # los costos menores a 1000 no se le agrega impuesto
+      total_price += cost  # los costos menores a 1000 no se le agrega impuesto
 
   return total_price
 
